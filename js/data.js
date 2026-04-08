166b25c440b9a81c3b4c5960025cd9fb3e491a2f
Facu91Cufre <facundocufre91@gmail.com>
2026-04-08 17:09:20 -0300

    Agrego js al proyecto

---
+++ js/data.js
@@ -0,0 +1,36 @@
+const categorias = ["Hamburguesas", "Pizzas", "Papas Fritas", "Bebidas"];
+
+const productos = [
+  {
+    id: 1,
+    nombre: "Hamburguesa Triple",
+    descripcion: "Triple carne, cheddar y bacon",
+    precio: 25000,
+    imagen: "./images/images.jpg",
+    categoria: "Hamburguesas",
+  },
+  {
+    id: 2,
+    nombre: "Pizza Muzzarella",
+    descripcion: "Salsa casera y orégano",
+    precio: 18000,
+    imagen: "./images/pizza.jpg",
+    categoria: "Pizzas",
+  },
+  {
+    id: 3,
+    nombre: "Papas Fritas",
+    descripcion: "Papas Fritas con cheddar",
+    precio: 10000,
+    imagen: "./images/papas.png",
+    categoria: "Papas Fritas",
+  },
+  {
+    id: 4,
+    nombre: "Coca Cola lata",
+    descripcion: "Lata de Coca Cola 354ml",
+    precio: 7000,
+    imagen: "./images/coca.jpg",
+    categoria: "Bebidas"
+  }
+];
---
+++ js/main.js
@@ -0,0 +1,35 @@
+const cargarCategorias = () => {
+  const lista = document.getElementById("lista-categorias");
+  categorias.forEach((categoria) => {
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+    a.href = "#";
+    a.textContent = categoria;
+    li.append(a);
+    lista.append(li);
+  });
+};
+
+const cargarProductos = () => {
+  const contProductos = document.getElementById("contenedor-productos");
+  productos.forEach((producto) => {
+    const article = document.createElement("article");
+    article.classList.add("card-producto");
+    article.innerHTML = `
+            <img src="${producto.imagen}" alt="${producto.nombre}" class="card-producto__imagen">
+            <h3>${producto.nombre}</h3>
+            <p>${producto.descripcion}</p>
+            <p>Precio: ${producto.precio}</p>
+            <button>Ver Detalles</button>
+            <button class="btn-agregar">Agregar al Carrito</button>
+        `;
+    const btnAgregar = article.querySelector(".btn-agregar");
+    btnAgregar.addEventListener("click", () => {
+      alert(`Agregaste ${producto.nombre} al carrito`);
+    });
+    contProductos.appendChild(article);
+  });
+};
+
+cargarCategorias();
+cargarProductos();
